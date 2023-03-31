9b628ea2ca9aed1a299b46c6c84e4b3ce8c30f25
Daniel Irwin <danielirwin99@gmail.com>
2023-03-31 16:44:35 +1100

    Messing around with a sidebar --> not successful

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Sidebar = (props: any) => {
   return (
-    <div className="h-[calc(100vh-80px)] w-1/5 bg-slate-500 ml-7 my-10">
-      <div></div>
+    <div className="hidden lg:flex relative h-[calc(80vh)] w-1/4 lg:w-1/5 bg-slate-200 ml-7 my-10 rounded-md">
+      <div className="absolute w-full h-6 bg-black rounded-t-md"></div>
     </div>
   );
 };
--- src/pages/TopSellers.tsx
+++ src/pages/TopSellers.tsx
@@ -4,13 +4,12 @@ import React from "react";
 const TopSellers = () => {
   return (
     <>
-      <Sidebar>
-        <div className="h-[calc(100vh-80px)]">
-          <div className="text-center mt-10 text-4xl font-bold">
-            <h1>Top Sellers</h1>
-          </div>
+      {/* <Sidebar /> */}
+      <div className="h-[calc(100vh-80px)]">
+        <div className="text-center mt-10 text-4xl font-bold">
+          <h1>Top Sellers</h1>
         </div>
-      </Sidebar>
+      </div>
     </>
   );
 };
